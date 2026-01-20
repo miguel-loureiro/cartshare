2d1549a33449f89d74b036c678e2622e36179274
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2026-01-20 22:49:21 +0000

    removed unused imports

--- backend/src/main/java/com/cartshare/backend/core/model/Category.java
+++ backend/src/main/java/com/cartshare/backend/core/model/Category.java
@@ -3,9 +3,6 @@ package com.cartshare.backend.core.model;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.cloud.spring.data.firestore.Document;
 import lombok.NonNull;
-import org.springframework.data.annotation.Id;
-
-import java.util.List;
 
 @Document(collectionName = "categories")
 public record Category(
